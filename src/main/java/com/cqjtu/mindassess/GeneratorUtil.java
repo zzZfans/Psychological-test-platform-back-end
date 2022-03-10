41e5a2b4f05a2e8703360f854c2df8d75be11090
zhangzhenc <zhangzhencheng1598@163.com>
2022-03-10 23:16:21 +0800

    初始化

---
+++ src/main/java/com/cqjtu/mindassess/GeneratorUtil.java
@@ -0,0 +1,74 @@
+package com.cqjtu.mindassess;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+
+/**
+ * @author: author
+ * @date: 2022/3/10
+ */
+public class GeneratorUtil {
+
+    public static void main(String[] args) {
+
+        String[] tables = new String[]{"user"};
+
+        // 1、创建代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+        // 2、全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        // 项目输出根目录
+        gc.setOutputDir(projectPath + "/src/main/java");
+        // 作者
+        gc.setAuthor("author");
+        //生成后是否打开资源管理器
+        gc.setOpen(false);
+        //主键策略
+        gc.setIdType(IdType.AUTO);
+        mpg.setGlobalConfig(gc);
+
+        // 3、数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://127.0.0.1:3306/book?serverTimezone=GMT%2B8&characterEncoding=utf-8");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("zzc123");
+        dsc.setDbType(DbType.MYSQL);
+        mpg.setDataSource(dsc);
+
+        // 4、包配置
+        PackageConfig pc = new PackageConfig();
+        // 生成的文件放在那个目录下  若没有该文件 则会创建
+        pc.setParent("com.cqjtu.");
+        pc.setModuleName("mindassess");
+        pc.setController("controller");
+        pc.setService("service");
+        pc.setServiceImpl("service.impl");
+        pc.setMapper("mapper");
+        pc.setEntity("entity");
+        mpg.setPackageInfo(pc);
+
+        // 5、策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        //数据库表映射到实体的命名策略
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        //数据库表字段映射到实体的命名策略
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        // lombok
+        strategy.setEntityLombokModel(true);
+        //restful api风格控制器
+        strategy.setRestControllerStyle(true);
+
+        //包括的表
+        strategy.setInclude(tables);
+        mpg.setStrategy(strategy);
+
+        // 6、执行
+        mpg.execute();
+    }
+}
---
+++ src/main/java/com/cqjtu/mindassess/MindAssessApplication.java
@@ -0,0 +1,16 @@
+package com.cqjtu.mindassess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author author
+ */
+@SpringBootApplication
+public class MindAssessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindAssessApplication.class, args);
+    }
+
+}
